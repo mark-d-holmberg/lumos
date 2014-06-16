3a51e7370dc3ff2f5d00093502c1e47d5887fdd9
Mark D Holmberg <mark.d.holmberg@gmail.com>
2014-06-16 11:17:07 -0600

    Creates a seeds file for the US States. #21.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,64 @@
 # Create the default TOS
 Tos.create!(content: "Terms and Conditions")
 
+# Create an admin user
 admin = User.create!(email: 'mark.d.holmberg@gmail.com', password: 'change_me_123', password_confirmation: 'change_me_123')
+
+# Populate the states
+states = [
+  ["Alabama","AL"],
+  ["Alaska","AK"],
+  ["Arizona","AZ"],
+  ["Arkansas","AR"],
+  ["California","CA"],
+  ["Colorado","CO"],
+  ["Connecticut","CT"],
+  ["Delaware","DE"],
+  ["Florida","FL"],
+  ["Georgia","GA"],
+  ["Hawaii","HI"],
+  ["Idaho","ID"],
+  ["Illinois","IL"],
+  ["Indiana","IN"],
+  ["Iowa","IA"],
+  ["Kansas","KS"],
+  ["Kentucky","KY"],
+  ["Louisiana","LA"],
+  ["Maine","ME"],
+  ["Montana","MT"],
+  ["Nebraska","NE"],
+  ["Nevada","NV"],
+  ["New Hampshire","NH"],
+  ["New Jersey","NJ"],
+  ["New Mexico","NM"],
+  ["New York","NY"],
+  ["North Carolina","NC"],
+  ["North Dakota","ND"],
+  ["Ohio","OH"],
+  ["Oklahoma","OK"],
+  ["Oregon","OR"],
+  ["Maryland","MD"],
+  ["Massachusetts","MA"],
+  ["Michigan","MI"],
+  ["Minnesota","MN"],
+  ["Mississippi","MS"],
+  ["Missouri","MO"],
+  ["Pennsylvania","PA"],
+  ["Rhode Island","RI"],
+  ["South Carolina","SC"],
+  ["South Dakota","SD"],
+  ["Tennessee","TN"],
+  ["Texas","TX"],
+  ["Utah","UT"],
+  ["Vermont","VT"],
+  ["Virginia","VA"],
+  ["Washington","WA"],
+  ["West Virginia","WV"],
+  ["Wisconsin","WI"],
+  ["Wyoming","WY"]
+]
+
+# Load em up
+states.each do |name, abbr|
+  State.find_or_create_by(name: name, abbr: abbr)
+end
