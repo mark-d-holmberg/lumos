264087c0e518884f4f6a06a7487d75071983384a
Mark D Holmberg <mark.d.holmberg@gmail.com>
2014-06-25 11:16:48 -0600

    Updates the URL options in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Default host for the mailers
-  config.action_mailer.default_url_options = { host: "mylumos.com" }
+  config.action_mailer.default_url_options = { host: "golumos.com" }
 end
